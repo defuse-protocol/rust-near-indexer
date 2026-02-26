da55545725c8abc0f06701155bb1f0f5d7a993d8
Bohdan Khorolets <bogdan@khorolets.com>
2026-02-26 14:38:44 +0200

    Refactor PG migration to generate partitions with a loo

--- indexer-postgres/migrations/20250225000000_create_silver_dip4_transfers.sql
+++ indexer-postgres/migrations/20250225000000_create_silver_dip4_transfers.sql
@@ -40,102 +40,21 @@ CREATE INDEX IF NOT EXISTS idx_silver_dip4_transfers_receipt
     ON silver_dip4_transfers (related_receipt_id);
 
 -- Pre-create monthly partitions: Dec 2024 through Dec 2026
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2024_12
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2024-12-01') TO ('2025-01-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_01
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-01-01') TO ('2025-02-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_02
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-02-01') TO ('2025-03-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_03
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-03-01') TO ('2025-04-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_04
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-04-01') TO ('2025-05-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_05
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-05-01') TO ('2025-06-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_06
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-06-01') TO ('2025-07-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_07
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-07-01') TO ('2025-08-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_08
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-08-01') TO ('2025-09-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_09
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-09-01') TO ('2025-10-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_10
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-10-01') TO ('2025-11-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_11
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-11-01') TO ('2025-12-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2025_12
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2025-12-01') TO ('2026-01-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_01
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-01-01') TO ('2026-02-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_02
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-02-01') TO ('2026-03-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_03
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-03-01') TO ('2026-04-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_04
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-04-01') TO ('2026-05-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_05
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-05-01') TO ('2026-06-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_06
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-06-01') TO ('2026-07-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_07
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-07-01') TO ('2026-08-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_08
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-08-01') TO ('2026-09-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_09
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-09-01') TO ('2026-10-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_10
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-10-01') TO ('2026-11-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_11
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-11-01') TO ('2026-12-01');
-
-CREATE TABLE IF NOT EXISTS silver_dip4_transfers_2026_12
-    PARTITION OF silver_dip4_transfers
-    FOR VALUES FROM ('2026-12-01') TO ('2027-01-01');
+DO $$
+DECLARE
+    month_start DATE;
+BEGIN
+    FOR month_start IN
+        SELECT generate_series('2024-12-01'::date, '2026-12-01'::date, '1 month'::interval)::date
+    LOOP
+        EXECUTE format(
+            'CREATE TABLE IF NOT EXISTS silver_dip4_transfers_%s_%s
+                PARTITION OF silver_dip4_transfers
+                FOR VALUES FROM (%L) TO (%L)',
+            to_char(month_start, 'YYYY'),
+            to_char(month_start, 'MM'),
+            month_start,
+            (month_start + interval '1 month')::date
+        );
+    END LOOP;
+END $$;
